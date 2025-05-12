f18627a03cd46db6a6fabfdcc1fa1cbb68fe8f3c
GregoireBDN <gregoire.bodin04@gmail.com>
2025-05-12 14:28:33 +0200

    delet expo

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -4,7 +4,6 @@ import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
-import { UserModule } from './user/user.module';
 
 @Module({
   imports: [AuthModule, UserModule],
