ee44c7153aea8d3d2e009f216465e59773f2572b
GregoireBDN <gregoire.bodin04@gmail.com>
2025-05-13 11:31:41 +0200

    signup

--- apps/api/src/user/dto/create-user.dto.ts
+++ apps/api/src/user/dto/create-user.dto.ts
@@ -1,10 +1,7 @@
 import { IsEmail, IsString } from 'class-validator';
 export class CreateUserDto {
   @IsString()
-  firstName: string;
-
-  @IsString()
-  lastName: string;
+  name: string;
 
   @IsString()
   @IsEmail()
