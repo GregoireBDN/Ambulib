c2a5ff5c5c5e16c065b868aa011d5f273267f77b
GregoireBDN <gregoire.bodin04@gmail.com>
2025-05-13 12:55:33 +0200

    signin

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -1,6 +1,9 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from '../user/dto/create-user.dto';
+import { LocalAuthGuard } from './guard/local-auth/local-auth.guard';
+import { Request as ExpressRequest } from 'express';
+
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
@@ -8,5 +11,11 @@ export class AuthController {
   @Post('signup')
   registerUser(@Body() createUserDto: CreateUserDto) {
     return this.authService.registerUser(createUserDto);
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('signin')
+  loginUser(@Request() req: ExpressRequest) {
+    return req.user;
   }
 }
--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -1,15 +1,43 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { CreateUserDto } from '../user/dto/create-user.dto';
 import { UserService } from '../user/user.service';
-
+import { verify } from 'argon2';
 @Injectable()
 export class AuthService {
   constructor(private readonly userService: UserService) {}
+  /**
+   * Register a new user
+   * @param dto - The user data
+   * @returns The created user
+   * @throws ConflictException if the user already exists
+   */
   async registerUser(dto: CreateUserDto) {
     const user = await this.userService.findByEmail(dto.email);
     if (user) {
       throw new ConflictException('User already exists');
     }
     return this.userService.create(dto);
+  }
+
+  /**
+   * Validate a local user
+   * @param email - The user's email
+   * @param password - The user's password
+   * @returns The validated user
+   * @throws UnauthorizedException if the user is not found or the password is invalid
+   */
+  async validateLocalUser(email: string, password: string) {
+    const user = await this.userService.findByEmail(email);
+    if (!user) {
+      throw new UnauthorizedException('User not found');
+    }
+    if (!(await verify(user.password, password))) {
+      throw new UnauthorizedException('Invalid password');
+    }
+    return { id: user.id, name: user.name };
   }
 }
